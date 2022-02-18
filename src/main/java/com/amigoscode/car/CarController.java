c6f676cf02e1613332bd586c059e2da4d229a173
Rachel Kaur <rachel.km@outlook.com>
2022-02-18 02:00:54 +0000

    Initial commit

---
+++ src/main/java/com/amigoscode/car/CarController.java
@@ -0,0 +1,28 @@
+package com.amigoscode.car;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CarController {
+
+    private CarService carService;
+
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
+    @PostMapping(path = "cars")
+    public void addCar(@RequestBody Car car) {
+        carService.registerNewCar(car);
+    }
+
+    @GetMapping(path = "cars")
+    public List<Car> getCars() {
+        return carService.getCars();
+    }
+}
